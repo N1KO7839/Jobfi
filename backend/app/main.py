c3cebe480d8a788c533d133e1956bc34ec94ac1d
N1KO7839 <nikodemkarla@gmail.com>
2026-01-08 10:20:55 +0100

    feat: create first simple scraper for JustJoinIT without filters etc.

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from scrapers.JustJoinItScraper import JustJoinItScraper
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    scraperJustJoin = JustJoinItScraper(5)
+    jobOffers = await scraperJustJoin.scrape("https://justjoin.it/job-offers/remote?workplace=hybrid&with-salary=yes&orderBy=DESC&sortBy=published")
+    return jobOffers
