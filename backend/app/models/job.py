283eccf90aafcb87449c918154296f8908551edc
N1KO7839 <nikodemkarla@gmail.com>
2026-04-19 23:36:13 +0200

    feat: update tables models to make migration possible without errors

--- backend/app/models/job.py
+++ backend/app/models/job.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from decimal import Decimal
 from typing import Literal
-from sqlalchemy import TIMESTAMP, Column, text
+from sqlalchemy import TIMESTAMP, Column, text, String
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlmodel import Field, SQLModel
 from uuid import UUID, uuid4
 
@@ -10,9 +11,9 @@ class JobOffer(SQLModel, table=True):
     title: str
     company: str
     salary: Decimal = Field(default=0, nullable=False)
-    tech_stack: list[str]
+    tech_stack: list[str] = Field(sa_column=Column(ARRAY(String)))
     location: str | None = None
-    workingMode: Literal["Remote", "Hybrid", "Office"]
+    workingMode: Literal["Remote", "Hybrid", "Office"] = Field(sa_column=Column(String))
     url: str
     created_datetime: datetime = Field(sa_column=Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text("now()")))
